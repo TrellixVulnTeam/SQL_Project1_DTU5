1f76d45ea66109a20c9651501c266900c9303538
Nicholas Reynolds <nick.b.reyn@gmail.com>
2022-07-11 14:36:51 -0500

    phase 6 complete. Project finished. wrote a query that updates a field in the table with a new value

--- sql-files/phase-6.sql
+++ sql-files/phase-6.sql
@@ -1,2 +1,9 @@
 -- Update the toy with the name of "Cheetos" to have a name of "Pooky"
--- Your code here
+UPDATE toys
+SET name = 'Pooky'
+WHERE cat_id IN (SELECT id FROM cats WHERE name = 'Garfield');
+
+--This also works to change the name but is less specific and could cause problems if there were multiple records with the same value.
+--UPDATE toys
+--SET name = 'Pooky'
+--WHERE name = 'Cheetos';
